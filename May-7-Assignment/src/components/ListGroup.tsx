1c835d3f8bcb7e61faabccc18daa1595701d5fc7
lheafey <lheafey@bates.edu>
2024-05-07 11:08:57 -0400

    initial Upload

---
+++ May-7-Assignment/src/components/ListGroup.tsx
@@ -0,0 +1,19 @@
+
+function ListGroup(){
+    let items = [
+        'New York',
+        'San Francisco',
+        'Tokyo',
+        'London',
+        'Paris'
+    ]; 
+    items = []
+    return(<>
+        <h1>List</h1>
+                {items.length === 0 && <p> No Item Found </p>}
+         <ul className="list-group">
+        {items.map(item => <li key={item}>{item}</li>)}
+          </ul>
+    </>);
+}
+export default ListGroup;
