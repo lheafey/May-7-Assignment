eaa52aa3c735cc3c51868dfa1007198a1e822526
lheafey <lheafey@bates.edu>
2024-05-07 11:54:22 -0400

    40 min in

--- May-7-Assignment/src/components/ListGroup.tsx
+++ May-7-Assignment/src/components/ListGroup.tsx
@@ -7,12 +7,15 @@ function ListGroup(){
         'London',
         'Paris'
     ]; 
-    items = []
+
     return(<>
         <h1>List</h1>
                 {items.length === 0 && <p> No Item Found </p>}
          <ul className="list-group">
-        {items.map(item => <li key={item}>{item}</li>)}
+        {items.map(item => <li className = "list-group-item"
+        key={item}
+        onClick={(event) => console.log(item, index)}
+         >{item}</li>)}
           </ul>
     </>);
 }
